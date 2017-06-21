695bdeb8b75fb37ad4808bbc6cb3db9a5ba65d62
Joe Wood <joewoodconsulting@outlook.com>
2017-06-21 17:32:32 -0400

    Adding header/trailer for FIX5

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -31,6 +31,7 @@ public class App {
         case "INT":
         case "LENGTH":
         case "NUMINGROUP":
+        case "SEQNUM":
             return "long";
         case "BOOLEAN":
             return "boolean";
@@ -59,50 +60,64 @@ public class App {
         return sb.toString();
     }
 
-/** Returns an avro field definition for a specified Fix Field and Fix Type (converting to avro primitives) */
+    /** Returns an avro field definition for a specified Fix Field and Fix Type (converting to avro primitives) */
     public static String fieldLine(String fieldName, int fixTag, String fieldType, String subType, boolean optional,
             boolean array) {
         ArrayList<String> fields = new ArrayList<>();
         fields.add("\"name\": \"" + fieldName.substring(0, 1).toLowerCase() + fieldName.substring(1) + "\" ");
         String type = subType != null ? subType : ("\"" + decodeType(fieldType) + "\"");
+        fields.add("\"fixTag\": " + fixTag);
+        fields.add("\"fixType\": \"" + fieldType + "\"");
         if (array) {
-            fields.add("\"items\": " + type);
+            // fields.add("\"items\": " + type);
             type = "{ \"type\":\"array\",\"items\": " + type + "}";
         }
         if (optional) {
             type = "[ \"null\", " + type + " ]";
             fields.add("\"default\": null");
         }
-        fields.add("\"fixTag\": " + fixTag);
-        fields.add("\"fixType\": \"" + fieldType + "\"");
         fields.add("\"type\": " + type);
         return "{ " + String.join(", ", fields) + " }";
     }
 
-/** returns an Avro Schema Type for the supplied set of fields in the data dictionary */
-    private static String createType(String name, quickfix.DataDictionary dict, int[] fields, String messageId,
-            HashMap<String, String> typeCache, int indent) {
+    /** returns an Avro Schema Type for the supplied set of fields in the data dictionary */
+    private static String createType(String name, quickfix.DataDictionary dict, quickfix.DataDictionary dictFixt,
+            int[] fields, String messageId, HashMap<String, String> typeCache, int indent) {
         java.util.List<String> output = new java.util.LinkedList<>();
         for (int i : fields) {
-            String fieldName = dict.getFieldName(i);
-            Boolean required = dict.isRequiredField(messageId, i);
-            quickfix.FieldType ee = dict.getFieldTypeEnum(i);
+            boolean isHeader = dictFixt.isHeaderField(i);
+            boolean isTrailer = dictFixt.isTrailerField(i);
+            DataDictionary useDict = (isHeader || isTrailer) ? dictFixt : dict;
 
-            Boolean group = dict.isGroup(messageId, i);
-            String fieldTypeName = ee == null ? "UNKNOWN" : ee.getName();
+            String fieldName = useDict.getFieldName(i);
+            Boolean required = isHeader ? useDict.isRequiredHeaderField(i)
+                    : isTrailer ? useDict.isRequiredTrailerField(i) : useDict.isRequiredField(messageId, i);
+            quickfix.FieldType ee = useDict.getFieldTypeEnum(i);
+
+            Boolean group = isHeader ? useDict.isHeaderGroup(i) : useDict.isGroup(messageId, i);
+            String fieldTypeName = (ee == null) ? ("NOTFOUND:" + useDict.getFieldType(i)) : ee.getName();
             String subType = null;
             boolean isArrayField = isArray(fieldTypeName);
+            if (ee.getName() == "UNKNOWN") {
+                fieldTypeName = "string";
+            }
             if (group) {
-                GroupInfo groupInfo = dict.getGroup(messageId, i);
+                GroupInfo groupInfo = isHeader ? useDict.getGroup(DataDictionary.HEADER_ID, i)
+                        : isTrailer ? useDict.getGroup(DataDictionary.TRAILER_ID, i) : useDict.getGroup(messageId, i);
                 fieldTypeName = fieldName.substring(2) + "Type";
                 if (!typeCache.containsKey(fieldTypeName)) {
                     DataDictionary ddd = groupInfo.getDataDictionary();
-                    subType = createType(fieldTypeName, dict, ddd.getOrderedFields(), messageId, typeCache, indent + 2);
+                    subType = createType(fieldTypeName, dict, dictFixt, ddd.getOrderedFields(), messageId, typeCache,
+                            indent + 2);
                     typeCache.put(fieldTypeName, subType);
                 }
             }
+            if (group && fieldName.startsWith("No")) {
+                fieldName = fieldName.substring(2,3).toLowerCase() + fieldName.substring(3);
+            }
             String fieldLineDef = fieldLine(fieldName, i, fieldTypeName, subType, !required, group || isArrayField);
-            output.add(getTabs(indent + 1) + fieldLineDef);
+            output.add(getTabs(indent + 1) + fieldLineDef
+                    + (ee.getName() == "UNKNOWN" ? "// Warning - UNKNOWN TYPE default to String" : ""));
         }
         return "{\n" + getTabs(indent) + "\"type\": \"record\",\n" + getTabs(indent) + "\"name\": \"" + name + "\",\n"
                 + getTabs(indent) + "\"fields\": [\n" + String.join(",\n", output) + "\n" + getTabs(indent) + "]\n"
@@ -113,18 +128,22 @@ public class App {
         try {
             if (args.length == 0 || args[0] == "--help") {
                 System.out.println("Use fix-avro <message-name> [FIX44|FIX50]");
-                return;
+                // return;
             }
-            final String vers = (args.length > 1) ? args[1] : "FIX50";
-            final String messageName = args[0];
+            final String vers = (args.length > 1) ? args[1] : "FIX50SP2";
+            final String messageName = (args.length > 0) ? args[0] : "TradeCaptureReport";
+
             quickfix.DataDictionary dict = new DataDictionary(vers + ".xml");
+            quickfix.DataDictionary fixtDict = new DataDictionary("FIXT11.xml");
+
             HashMap<String, String> typeCache = new HashMap<>();
             final String messageId = dict.getMsgType(messageName);
-            // quickfix.fix50sp2.NewOrderSingle dd = new quickfix.fix50sp2.NewOrderSingle();
-            IntStream orderedFields = IntStream.of(dict.getOrderedFields());
-            int[] filtered = orderedFields.filter(i -> dict.isMsgField(messageId, i)).toArray();
 
-            String output = createType(messageName, dict, filtered, messageId, typeCache, 1);
+            int[] filtered = IntStream.of(dict.getOrderedFields()).filter(
+                    i -> dict.isMsgField(messageId, i) || fixtDict.isHeaderField(i) || fixtDict.isTrailerField(i))
+                    .toArray();
+
+            String output = createType(messageName, dict, fixtDict, filtered, messageId, typeCache, 1);
             System.out.println(output);
         } catch (quickfix.ConfigError e) {
             System.out.format("Error " + e.getMessage());
